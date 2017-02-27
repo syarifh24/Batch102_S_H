a0914471f93947ddd910db5d5a29ae1fbcdf06ad
syarifh24 <syarifh24@gmail.com>
2017-02-27 16:58:24 +0700

    Upload 27/02/2017

---
+++ day02/Logicday02/logicday02/logicday02/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicday02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Masukan No Soal : ");
+                    int soal = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Masukan Angka : ");
+                    int n = int.Parse(Console.ReadLine()); //deklarasi n agar jumlah bintang y bisa berubah sesuai yg kita input
+                    //bisa di tambah method ke 2 contoh --> String nama = "Xsis";
+                    switch (soal)
+                    {
+                        case 1: soal1(n); break;
+                        case 2: soal2(n); break;
+                        case 3: soal3(n); break;
+                        default:
+                            break;
+                    }
+
+                    Console.Write("Apakah Ingin Melanjutkan ? ");
+                    string tanya = Console.ReadLine();
+                    if (tanya.ToUpper() != "Y")
+                    {
+                        break;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                    Console.ReadLine();
+                    continue;
+                }
+
+            }
+
+        }
+        
+        public static void soal1(int n)
+        {
+            //int n = 9;
+            for (int i = 0; i < n; i++)
+           {
+               for (int j = 0; j < n; j++)
+               {
+                   if (i==j || i + j == n - 1 )
+                   {
+                       Console.Write("*\t");
+                   }
+                   else
+                   {
+                       Console.Write("\t");
+                   }
+
+               }
+               Console.WriteLine("\n\n\n");
+           }
+
+        }
+        
+        public static void soal2(int n)
+        {
+            //int n = 9;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i + j == n - 1)
+                    {
+                        Console.Write("*\t");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+
+                }
+                Console.WriteLine("\n\n\n");
+            }
+        }
+        public static void soal3(int n) // int n ini disebut method bisa lebih dari 1 contoh soal3(int n,String nama)
+        {
+            //int n = 9; jika dideklarasikan disini makan n teteap berjumlah 9
+            //cetak String Nama disini --> console.writeline("nama")
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j || i + j == n - 1)
+                    {
+                        Console.Write("*\t");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+
+                }
+                Console.WriteLine("\n\n\n");
+            }
+            //Console.ReadKey;
+        }
+    }
+}
---
+++ day02/logic2day02/logic2day02/logic2day02/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic2day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan No Soal : ");
+            int soal = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukan Jumlah Array : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n\n");
+
+            switch (soal)
+            {
+                case 1: soal1(n); break;
+                case 2: soal2(n); break;
+                case 3: soal3(n); break;
+                case 4: soal4(n); break;
+                case 5: soal5(n); break;
+                case 6: soal6(n); break;
+                case 7: soal7(n); break;
+                case 8: soal8(n); break;
+                case 9: soal9(n); break;
+                case 10: soal10(n); break;
+
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        
+        //1--------------------------------------------------------
+        public static void soal1(int n)
+        {
+            int[] ganjil = new int[n];
+            int a = 1;
+            
+            for (int i=0;i<n;i++)
+            {
+                  
+                   ganjil[i]=a;
+                   a=a+2;
+                  
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+
+                    if (j == k)
+                    {
+                        Console.Write(ganjil[j]+"\t\t");
+                        
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+
+                }
+                Console.WriteLine("\n\n");
+            }
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal2(int n)
+        {
+            int[] genap = new int[n];
+            int b = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                genap[i] = b;
+                b = b + 2;
+
+            }
+
+            int z = 0;
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                   
+                    if (j+k==n-1)
+                    {
+                        z = n - (j+1);
+                        Console.Write(genap[z] + "\t\t");
+                    }   
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+
+                }
+                Console.WriteLine("\n\n");
+            }
+        
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal3(int n)
+        {
+            int[] ganjil = new int[n];
+            int a = 1;
+            int[] genap = new int[n];
+            int b = 0;
+            int z = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                ganjil[i] = a;
+                a = a + 2;
+                genap[i] = b;
+                b = b + 2;
+
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+
+                    if (j == k || j + k == n - 1)
+                    {
+                        Console.Write(ganjil[j] );
+                        
+                        z = n - (j + 1);
+                        Console.Write(genap[z] + "\t");
+                        
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+
+                }
+                Console.WriteLine("\n\n");
+            }
+
+
+            //batas---------------------------------
+
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal4(int n)
+        {
+
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal5(int n)
+        {
+
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal6(int n)
+        {
+
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal7(int n)
+        {
+
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal8(int n)
+        {
+
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal9(int n)
+        {
+
+        }
+
+
+        //--------------------------------------------------------
+        public static void soal10(int n)
+        {
+
+        }
+    }
+}
